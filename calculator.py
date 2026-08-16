641e21914365873a3ab7da978d62668a380a219a
Michael <barkerbjones@outlook.com>
2026-08-15 22:30:46 -0600

    Refractored, cleaned up main and added modules

---
+++ calculator.py
@@ -0,0 +1,19 @@
+import math
+
+class Formula():
+    def __init__(self, bore, rod, pressure):
+        self.bore = bore
+        self.rod = rod
+        self.pressure = pressure
+
+    def piston_area(self):
+        return math.pi * self.bore**2 / 4
+
+    def annulus(self):
+        return math.pi * self.bore**2 / 4 - math.pi * self.rod**2 / 4
+
+    def cyl_ext_force(self):
+        return self.pressure * self.piston_area()
+
+    def cyl_ret_force(self):
+        return self.pressure * self.annulus()
